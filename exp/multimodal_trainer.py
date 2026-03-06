a1e79c8c442aa0a9b54b52084fde1b7855555d62
RezkyKam50 <rezkykam34@gmail.com>
2026-03-07 02:50:18 +0700

    update

--- exp/multimodal_trainer.py
+++ exp/multimodal_trainer.py
@@ -509,6 +509,12 @@ def main(args):
             skip_attn_scheme="SHUFFLE",
             end_attn_scheme="COORD",
         ),
+        "DSUnetCoord_COORD": DSGhostUnet(
+            cfg=Config_DSUnet, 
+            use_prithvi=False,
+            skip_attn_scheme="COORD",
+            end_attn_scheme="COORD",
+        ),
         "DSUnetShuffle_SHUFFLE": DSGhostUnet(
             cfg=Config_DSUnet, 
             use_prithvi=False,
